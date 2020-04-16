22ae57c75d558ec324fd6e5915050fc6feb7de30
Arthur <gataullin12@mail.ru>
2020-04-17 00:01:22 +0300

    question component changed

--- src/pages/PreferenceLab/components/Question.vue
+++ src/pages/PreferenceLab/components/Question.vue
@@ -1,34 +1,36 @@
 <template>
   <div>
-    <div class = 'goal'>
-      <h5>{{ question.question }}</h5>
-      <img v-if="question.path" :src="question.path" />
-        <div v-if='!done'>
-          <div v-for="(item, index) in question.answer" v-bind:key='index'>
-            <q-checkbox  v-model='arr[index]' :label='item'/>
-          </div>
-          <p v-if='validation' style='color: red'>Выберите, по крайней мере, один вариант ответа</p>
-          <p v-if='err' style='color: red'>Неверно</p>
-          <q-btn class='bu' @click='check()' label='Проверить' color="secondary"/>
-        </div>
-        <div v-if='done'>
-          <p v-for="(item, index) in question.answer" v-bind:key='index'>{{ item }}</p>
-          <p v-if='err' style="font-size: 16px; color: red">Вы допустили ошибку</p>
-          <p v-else style="font-size: 16px; color: green">Верно</p>
-        </div>
+    <h5>{{ question.question }}</h5>
+    <img v-if="question.path" :src="question.path" />
+    <div v-if='!done'>
+      <div v-for="(item, index) in question.answer" v-bind:key='index'>
+        <q-checkbox  v-model='arr[index]' :label='item'/>
+      </div>
+      <p v-if='validation' style='color: red'>Выберите, по крайней мере, один вариант ответа</p>
+      <p v-if='err' style='color: red'>Неверно</p>
+      <q-btn class='bu' @click='check()' label='Проверить' color="secondary"/>
+    </div>
+    <div v-if='done'>
+      <p v-for="(item, index) in question.answer" v-bind:key='index'>{{ item }}</p>
+      <p v-if='err' style="font-size: 16px; color: red">Вы допустили ошибку</p>
+      <p v-else style="font-size: 16px; color: green">Верно</p>
+      <button-next label='Продолжить'/>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['question', 'done'],
+  props: ['question', 'done', 'mark'], // получаем на вход в вашем случае ошибку либо её отсутствие
   data () {
     return {
       validation: false,
       arr: [false, false, false, false],
       err: false
     }
+  },
+  created () {
+    this.err = this.mark // переопреляем её в случае необходимости
   },
   methods: {
     check () {
@@ -37,10 +39,11 @@ export default {
         this.validation = true
       } else if (!this.arr[this.question.right] || !this.checkArr()) {
         this.$emit('error')
-        this.$emit('success')
         this.err = true
+        this.done = true
       } else {
-        this.$emit('success')
+        this.done = true
+        return true
       }
     },
     checkArr () {
