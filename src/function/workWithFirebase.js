46ff0385858de73c995a61d5b38679d918b495b3
Arthur <gataullin12@mail.ru>
2020-04-19 15:00:35 +0300

    variants for users

--- src/function/workWithFirebase.js
+++ src/function/workWithFirebase.js
@@ -2,13 +2,23 @@ import { generate } from './gen'
 import * as firebase from 'firebase'
 import variants from './variants'
 
-const usersWithVariants = ['-M3laaK6mM7HWas1EMDw']
+const usersWithVariants = {
+  '-M3laaK6mM7HWas1EMDw': 0,
+  '-M4Q1b80w6kBbGOWuk1G': 1,
+  '-M4Pn67BYX5Daensy7ve': 2,
+  '-M4PmenQPUPCZHL5rycY': 3,
+  '-M4PmZCB_KCPLK53e1ml': 6,
+  '-M3Ad--2oHBITF0JqmKA': 7,
+  '-M4Pnb2MQr6eNKSs4T7O': 10,
+  '-M4PmnGjorSfmhM1Yr3z': 11,
+  '-M4PmxFijtvj0uC70zFQ': 12,
+  '-M3D7r6RZsgrQ0UeEg8P': 13
+}
 
 export function checkLab (labName) {
   let user = this.$store.getters['data/getUser']()
   let cid = this.$router.currentRoute.query.cid
   let work3 = this.$store.getters['data/getStudentWork3'](user.id, labName, cid)
-
   this.maxScore = this.$store.getters['data/getCourse'](cid).groups[user.group].lab3[work3.work.lab].maxScore
 
   if (work3) {
@@ -28,9 +38,12 @@ export function checkLab (labName) {
 
   firebase.database().ref('lab3/variants/1').once('value', snapshot => {
     let variant = snapshot.val()
-    let variantNumber = usersWithVariants.findIndex(function (e) { return e === user.id })
-    if (variantNumber !== -1) {
+    // let variantNumber = usersWithVariants.findIndex(function (e) { return e === user.id })
+    let variantNumber = usersWithVariants[user.id]
+    if (variantNumber !== undefined) {
       this.condition = variants[variantNumber]
+    } else if (user.id === '-M3laaK6mM7HWas1EMDw') {
+      this.condition = variants[0]
     } else {
       this.condition = generate(variant, variant['dict1'], variant['dict2'])
     }
